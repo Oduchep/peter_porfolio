faa83a45835933a4b496f7c1687442ecb1f5fc3b
Peter Okerulu <oduchep@gmail.com>
2024-08-26 17:02:04 +0100

    imported assets

---
+++ public/assets/icons/index.ts
@@ -0,0 +1,19 @@
+import CSSIcon from './css-icon.svg';
+import HtmlIcon from './html-icon.svg';
+import JavascriptIcon from './javascript-icon.svg';
+import ReactIcon from './react-icon.svg';
+import TailwindIcon from './tailwind-icon.svg';
+import TypescriptIcon from './typescript-icon.svg';
+import GithubIcon from './github-icon.svg';
+import NextIcon from './next-icon.svg';
+
+export {
+  CSSIcon,
+  HtmlIcon,
+  JavascriptIcon,
+  ReactIcon,
+  TailwindIcon,
+  TypescriptIcon,
+  GithubIcon,
+  NextIcon,
+};
---
+++ public/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as SmokeImg } from './smoke.png';
