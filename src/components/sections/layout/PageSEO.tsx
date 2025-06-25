6b33c7bc46fa2ec1442d98bfb2bd8210517fa5c4
Peter Okerulu <oduchep@gmail.com>
2025-06-25 23:39:07 +0100

    Update PageSEO component with new site URL and logo references

--- src/components/sections/layout/PageSEO.tsx
+++ src/components/sections/layout/PageSEO.tsx
@@ -14,7 +14,7 @@ const PageSEO = ({
   description = 'Default site description',
   keywords = 'default, keywords',
 }: PageSEOProps) => {
-  const siteUrl = 'https://www.majfintech.com';
+  const siteUrl = 'https://www.oduchep.com';
   const pathname = usePathname();
   const cleanPath = pathname?.split('#')[0].split('?')[0];
   const seoUrl = siteUrl + (cleanPath === '/' ? '' : cleanPath);
@@ -31,16 +31,16 @@ const PageSEO = ({
       {/* Open Graph */}
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
-      <meta property='og:image' content='/assets/images/MAJ-logo.png' />
+      <meta property='og:image' content='/assets/images/name-logo.svg' />
       <meta property='og:type' content='website' />
       <meta property='og:url' content={seoUrl} />
-      <meta property='og:image:alt' content='MAJ Fintech Logo' />
+      <meta property='og:image:alt' content='Peter Okerulu Logo' />
 
       {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content='/assets/images/MAJ-logo.png' />
+      <meta name='twitter:image' content='/assets/images/name-logo.svg' />
 
       <link rel='icon' href='/favicon.ico' />
       <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
